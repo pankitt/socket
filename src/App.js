29297e5cb4ebeb3b9032331dc70ed616f803d70d
delfa07@bigmir.net <bujhm007*>
2019-11-05 21:44:14 +0200

    2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
 import {Switch, Route}  from 'react-router-dom';
 import './App.css';
-import Menu from './Menu'
+import Menu from './Menu';
 import SocketPage from './SocketPage';
 
 import { SocketContext, socketReducer } from './shared/socket';
@@ -9,6 +9,20 @@ import { SocketContext, socketReducer } from './shared/socket';
 
 export default () => {
   const [socket, socketDispatch] = useReducer(socketReducer, {});
+  const [list, setList] = useState({});
+  console.log('list', list);
+
+  const setStorage = (id) => {
+    if (list && Object.keys(list).find(x => +x === id)) {
+      return setList(prevState => {
+        const state = { ...prevState };
+        delete state[id];
+        return state;
+      });
+    }
+    // {...list, [id]:id}
+    return setList(prevState => ({...prevState, [id]:id}));
+  };
 
   return (
     <>
@@ -21,6 +35,12 @@ export default () => {
           </SocketContext.Provider>
         </Route>
       </Switch>
+      <div className={'App'}>
+        <button onClick={() => setStorage(10)}>10</button>
+        <button onClick={() => setStorage(20)}>20</button>
+        <button onClick={() => setStorage(30)}>30</button>
+      </div>
+
     </>
   );
 };
